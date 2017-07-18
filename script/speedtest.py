658caf3fc1b39350ae66f0ffd742a661444d09c5
Paul Witt <pwitt@fanatics.com>
2017-07-18 11:56:14 -0400

    still testing

--- script/speedtest.py
+++ script/speedtest.py
@@ -1,80 +1,117 @@
+from collections import OrderedDict
 from dateutil.rrule import rrule, MINUTELY
+import bisect
 import calendar
 import datetime
-import bisect
+import json
 import matplotlib
 import matplotlib.pyplot as plt
-import json
+import numpy as np
 import os
 import pyspeedtest
 import sys
-import numpy as np
-from collections import OrderedDict
+
+## We need this mess to handle dates in json
+##
+def date_handler(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+def datetime_parser(dct):
+    for k, v in dct.items():
+        if isinstance(v, basestring):
+            try:
+                dct[k] = datetime.datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+            except:
+                pass
+    return dct
+
+## Folder setup
+##
+#folder_name = "D:/Development/speedtest"
+folder_name = "C:/DevGitHub/speedtest"
+data_file = os.path.join(folder_name, "data/weekly.json")
+image_file = os.path.join(folder_name, "images/image.png")
 
 ## Get the current 15 minute interval and date info. We need
 ## this for storing in the json
 ##
 times = list(rrule(MINUTELY,interval=15,dtstart=datetime.date.today(),count=96))
-rounded_time = times[bisect.bisect(times,datetime.datetime.now())]
+this_interval = times[bisect.bisect(times,datetime.datetime.now())]
+date_cutoff = this_interval - datetime.timedelta(days=7)
+twenty_four_ago = this_interval - datetime.timedelta(days=1)
 
-day_of_week = calendar.day_abbr[datetime.datetime.today().weekday()]
-this_hour = rounded_time.hour
-this_fifteen = rounded_time.minute
-this_interval = day_of_week + "-" + str(this_hour) + ":" + str(this_fifteen)
 
 ## Load the json file if it exists. Create an empty structure
 ## if it doesn't.
 ##
-folder_name = "C:/DevGitHub/speedtest"
-data_file = os.path.join(folder_name, "data/weekly.json")
-
 if os.path.isfile(data_file):
     with open (data_file, "r") as text_file:
         data_file_string = text_file.read()
-        data = json.loads(data_file_string, object_pairs_hook=OrderedDict)
+        data = json.loads(data_file_string, object_hook=datetime_parser)
 else:
-    data = {}
+    data = []
 
 ## At the very least we now have an empty data file. Time to
 ## test our speed and store it.
 st = pyspeedtest.SpeedTest(host='atl.speedtest.sbcglobal.net', http_debug=0, runs=5)
-ping = st.ping()
 download = st.download()
 upload = st.upload()
 
-try:
-    test_interval = data[this_interval]
-except:
-    data[this_interval] = {}
+## Loop through the existing data and do two things:
+## a) If this interval has already been created update the values
+## b) Delete any rows that are more than 7 days old
+##
+found = False
+new_data = []
+for row in data:
+    if this_interval == row['date']:
+        found = True
+        row['download'] = download
+        row['upload'] = upload
+        new_data.append(row)
+    elif row['date'] <= date_cutoff:
+        print "deleting row"
+    else:
+        new_data.append(row)
 
-data[this_interval]['ping'] = ping
-data[this_interval]['download'] = download
-data[this_interval]['upload'] = upload
+if found == False:
+    this_obj = {}
+    this_obj['date'] = this_interval
+    this_obj['download'] = download
+    this_obj['upload'] = upload
+
+    new_data.append(this_obj)
+
+## Sort this thing in date order.  We'll need this later when
+## we're trying to graph it.
+##
+new_data.sort(key=lambda x: x['date'])
 
 ## Write out the data to file.
 ##
-data_output = json.dumps(data, indent=4, sort_keys=True)
-print "Writing out data file"
+data_output = json.dumps(new_data, default=date_handler, indent=4, sort_keys=True)
+#print "Writing out data file"
 with open(data_file, "w") as text_file:
     text_file.write(data_output)
 
 ## Calculate averages for each value.
 ##
-divide_by = len(data)
-total_ping = 0
+divide_by = len(new_data)
 total_download = 0
 total_upload = 0
-for data_interval in data:
-    total_ping += data[data_interval]['ping']
-    total_download += data[data_interval]['download']
-    total_upload += data[data_interval]['upload']
+for row in new_data:
+    total_download += row['download']
+    total_upload += row['upload']
 
-average_ping = total_ping / divide_by
 average_download = (total_download / divide_by) / (1024 * 1024)
 average_upload = (total_upload / divide_by) / (1024 * 1024)
     
-avg_download_msg = "Average download: %.2f MB/s" % (average_download)
-avg_upload_msg = "Average upload: %.2f MB/s" % (average_upload)
+avg_download_msg = "Average download (last 7 days): %.2f MB/s" % (average_download)
+avg_upload_msg = "Average upload (last 7 days): %.2f MB/s" % (average_upload)
+
+#print avg_download_msg
+#print avg_upload_msg
 
 ## Let's try to graph this.
 ##
@@ -83,14 +120,14 @@ downloads = []
 uploads = []
 point_counter = 0
 points = []
-for data_interval in data:
-    if day_of_week in data_interval:
-        if ':00' in data_interval:
-            labels.append(data_interval)
+for row in new_data:
+    if row['date'] >= twenty_four_ago:
+        if row['date'].minute == 0:
+            labels.append(row['date'])
         else:
             labels.append('')
-        downloads.append(data[data_interval]['download'] / (1024 * 1024))
-        uploads.append(data[data_interval]['upload'] / (1024 * 1024))
+        downloads.append(row['download'] / (1024 * 1024))
+        uploads.append(row['upload'] / (1024 * 1024))
         points.append(point_counter)
         point_counter += 1
 
@@ -98,7 +135,7 @@ plot_count = len(labels)
 ind = np.arange(plot_count)
 
 ax0 = plt.subplot(2, 1, 1)
-plt.ylabel('Download (MB/s)', fontdict={'fontsize':'8'})
+plt.ylabel('Download (MB/s)\n(last 24 hours)', fontdict={'fontsize':'8'})
 plt.xticks(ind, [])
 plt.yticks(fontsize=6)
 ax0.spines['top'].set_visible(False)
@@ -110,7 +147,7 @@ plt.title(avg_download_msg, loc='left', fontdict={'fontsize':'10'})
 plt.subplots_adjust(left=0.1, right=1.9, top=0.9, bottom=0.1)
 
 ax1 = plt.subplot(2, 1, 2)
-plt.ylabel('Upload (MB/s)', fontdict={'fontsize':'8'})
+plt.ylabel('Upload (MB/s)\n(last 24 hours)', fontdict={'fontsize':'8'})
 plt.xticks(rotation=70, horizontalalignment='right', fontsize=6)
 plt.yticks(fontsize=6)
 plt.xticks(ind, labels)
@@ -124,4 +161,4 @@ plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
 
 plt.tight_layout()
 
-plt.show()
+plt.savefig(image_file)
